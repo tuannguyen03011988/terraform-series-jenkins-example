edc2cec6f8317deaab046547c267d70423405a8e
tuannguyen03011988 <120710788+tuannguyen03011988@users.noreply.github.com>
2022-12-19 15:18:23 +0700

    Add files via upload

---
+++ s3-backend/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type = string
+  default = "ap-southeast-1"
+}
+
+variable "project" {
+  description = "The project namespace to use for unique resource naming"
+  default     = "terraform-series-tuan"
+  type        = string
+}
+
+variable "principal_arns" {
+  description = "A list of principal arns allowed to assume the IAM role"
+  default     = null
+  type        = list(string)
+}
